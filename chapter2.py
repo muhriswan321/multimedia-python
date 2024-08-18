aef3d37a073370ab6e0df118808e267aa3bcc614
muhriswan321 <105841104821@student.unismuh.ac.id>
2024-08-17 22:32:36 -0700

    chapter 3 selesai

--- chapter2.py
+++ chapter2.py
@@ -1,7 +1,12 @@
 from PIL import Image
 from PIL import ImageFilter
+from pydub import AudioSegment
 
-image = Image.open('logo.jpeg')
+##Manipulasi Gambar dengan Pillow
+# Memuat gambar
+image = Image.open('naruto.jpg')
+
+# Menyimpan gambar
 image.save('result.jpg')
 
 cropped_image = image.crop((10, 10, 200, 200))
@@ -13,5 +18,19 @@ resized_image.save('resized_result.jpg')
 filtered_image = resized_image.filter(ImageFilter.BLUR)
 filtered_image.save('filtered_result.jpg')
 
-if filtered_image.mode == 'RGBA':
-    filtered_image = filtered_image.convert('RGB')
+##Memuat dan Menyimpan File Audio
+# Memuat file audio
+audio = AudioSegment.from_file('lagu.mp3')
+
+# Menyimpan file audio
+audio.export('result.mp3', format='mp3')
+
+clipped_audio = audio[:10000]  # Mendapatkan 10 detik pertama
+clipped_audio.export('clipped_result.mp3', format='mp3')
+
+combined_audio = audio + clipped_audio
+combined_audio.export('combined_result.mp3', format='mp3')
+audio.export('result.wav', format='wav')
+
+louder_audio = audio + 10  # Meningkatkan volume sebesar 10dB
+louder_audio.export('louder_result.mp3', format='mp3')
---
+++ chapter3ImageAudio.py
@@ -0,0 +1,43 @@
+import tkinter as tk
+from tkinter import filedialog
+from pydub import AudioSegment
+from PIL import Image, ImageTk
+import subprocess
+import tempfile
+import os
+
+# Membuat jendela utama
+root = tk.Tk()
+root.title("Multimedia Application")
+
+# Memuat gambar menggunakan Pillow
+image = Image.open('naruto.jpg')
+photo = ImageTk.PhotoImage(image)
+
+# Definisikan fungsi untuk memutar musik
+def play_music():
+    file_path = filedialog.askopenfilename()
+    if file_path:
+        audio = AudioSegment.from_file(file_path)
+
+        # Membuat file sementara dengan delete=False
+        with tempfile.NamedTemporaryFile(suffix=".mp3", delete=False) as temp_audio_file:
+            temp_file_path = temp_audio_file.name
+            audio.export(temp_file_path, format="mp3")
+        
+        # Memainkan audio dengan ffplay
+        subprocess.call(['ffplay', '-nodisp', '-autoexit', temp_file_path])
+
+        # Menghapus file sementara setelah pemutaran selesai
+        os.remove(temp_file_path)
+
+# Membuat tombol untuk memutar musik
+play_button = tk.Button(root, text="Play", command=play_music)
+play_button.pack()
+
+# Membuat label untuk menampilkan gambar
+label = tk.Label(root, image=photo)
+label.pack()
+
+# Menjalankan loop acara Tkinter
+root.mainloop()
